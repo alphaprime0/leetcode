66eabbdb701b536ef08c03c27b02293907a14eb9
Anbo Chen <grothendick_motif@outlook.com>
2022-10-02 01:17:00 -0500

    1 two sum

--- twoSum.py
+++ twoSum.py
@@ -1,9 +1,16 @@
+"""
+1. Two Sum
+Easy
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+"""
 class Solution:
-  def twoSum(nums, target: List[int], int) -> List[int]:
-    lookup = {}
-    for i in range(len(nums)):
-      if nums[i] in lookup:
-        return [lookup[nums[i]], i]
-      else:
-        lookup[target - nums[i]] = i
-    return None
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        lookup = {}
+        for i in range(len(nums)):
+            if nums[i] in lookup:
+                return [lookup[nums[i]], i]
+            else:
+                lookup[target - nums[i]] = i
+        return None
